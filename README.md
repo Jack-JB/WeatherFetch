db626e1a7528209f6761e5847d80c8c9ea928e36
Jack Burrows <jackjb94@gmail.com>
2023-09-30 00:06:10 +0100

    Main file and readme.md

---
+++ README.md
@@ -0,0 +1,37 @@
+# Weather App
+This is a simple command-line weather app that uses the OpenWeatherMap API to display the current weather in a given location.
+
+## Prerequisites
+Before you can run this app, you'll need to sign up for an API key from OpenWeatherMap. You can do this by following these steps:
+
+1. Go to the OpenWeatherMap website and click "Sign Up" in the top right corner.
+
+2. Follow the prompts to create an account and sign in.
+
+3. Once you're signed in, go to the API keys page and click "Generate" to create a new API key.
+
+4. Copy the API key to your clipboard.
+
+## Installation
+
+To install this app, follow these steps:
+
+1. Clone this repository to your local machine.
+
+2. Install the required dependencies by running go get in the project directory:
+> go get github.com/joho/godotenv
+
+3. Create a .env file in the project directory and add your OpenWeatherMap API key to it:
+> OPENWEATHERMAP_API_KEY=your_api_key_here
+
+Replace your_api_key_here with the API key you generated earlier.
+
+## Usage
+To use this app, run the following command in the project directory:
+> go run main.go
+
+This will display the current weather in London by default. To get the weather for a different location, modify the location variable in main.go to the desired location.
+
+## License
+
+TODO: add a license
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+type WeatherData struct {
+	Weather []struct {
+		Description string `json:"description"`
+	} `json:"weather"`
+	Main struct {
+		Temp float64 `json:"temp"`
+	} `json:"main"`
+	Sys struct {
+		Sunrise int64 `json:"sunrise"`
+		Sunset  int64 `json:"sunset"`
+	} `json:"sys"`
+}
+
+func main() {
+	location := "London"
+	apiKey := os.Getenv("OPENWEATHERMAP_API_KEY")
+	if apiKey == "" {
+		fmt.Println("OPENWEATHERMAP_API_KEY environment variable not set")
+		os.Exit(1)
+	}
+
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", location, apiKey)
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var weatherData WeatherData
+	err = json.Unmarshal(data, &weatherData)
+	if err != nil {
+		panic(err)
+	}
+
+	tempCelsius := weatherData.Main.Temp - 273.15
+
+	sunriseTime := time.Unix(weatherData.Sys.Sunrise, 0).Format("15:04:05")
+	sunsetTime := time.Unix(weatherData.Sys.Sunset, 0).Format("15:04:05")
+
+	fmt.Printf("Weather: %s\n", weatherData.Weather[0].Description)
+	fmt.Printf("Temperature: %.2f°C\n", tempCelsius)
+	fmt.Printf("Sunrise: %s\n", sunriseTime)
+	fmt.Printf("Sunset: %s\n", sunsetTime)
+}
