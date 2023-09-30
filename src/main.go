76b1c0423ec8b92e95a45c25744454169f9f2af2
Jack Burrows <jackjb94@gmail.com>
2023-09-30 20:06:48 +0100

    Added goroutines to improve efficiency

--- src/main.go
+++ src/main.go
@@ -84,82 +84,96 @@ func main() {
 		os.Exit(1)
 	}
 
-	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", *location, apiKey)
+	done := make(chan bool)
+	go func() {
+		url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", *location, apiKey)
 
-	client := &http.Client{
-		Timeout: 10 * time.Second,
+		client := &http.Client{
+			Timeout: 10 * time.Second,
+		}
+
+		resp, err := client.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		data, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		var weatherData WeatherData
+		err = json.Unmarshal(data, &weatherData)
+		if err != nil {
+			panic(err)
+		}
+
+		tempCelsius := weatherData.Main.Temp - 273.15
+
+		sunriseTime := time.Unix(weatherData.Sys.Sunrise, 0).Format("15:04:05")
+		sunsetTime := time.Unix(weatherData.Sys.Sunset, 0).Format("15:04:05")
+
+		asciiSun := `
+          \   /
+           .-.
+        ‒ (   ) ‒
+           .-.
+          /   \
+      `
+		asciiCloud := `
+            .--.
+         .-(    ).
+        (___.__)__)
+      `
+		acsiiRainCloud := `
+          .--.
+       .-(    ).
+      (___.__)__)
+       ʻ‚ʻ‚ʻ‚ʻ‚ʻ
+      `
+		asciiSnow := `
+          .--.
+       .-(    ).
+      (___.__)__)
+       * * * * *
+      `
+
+		asciiThunder := `
+         .--.
+      .-(    ).
+     (___.__)__)
+          /_
+           /
+      `
+
+		if strings.Contains(weatherData.Weather[0].Description, "sun") {
+			fmt.Println(asciiSun)
+		} else if strings.Contains(weatherData.Weather[0].Description, "rain") {
+			fmt.Println(acsiiRainCloud)
+		} else if strings.Contains(weatherData.Weather[0].Description, "snow") {
+			fmt.Println(asciiSnow)
+		} else if strings.Contains(weatherData.Weather[0].Description, "thunder") {
+			fmt.Println(asciiThunder)
+		} else {
+			fmt.Println(asciiCloud)
+		}
+
+		fmt.Printf("Location: %s\n", *location)
+		fmt.Printf("Weather: %s\n", weatherData.Weather[0].Description)
+		fmt.Printf("Temperature: %.2f°C\n", tempCelsius)
+		fmt.Printf("Sunrise: %s\n", sunriseTime)
+		fmt.Printf("Sunset: %s\n", sunsetTime)
+
+		done <- true
+	}()
+
+	select {
+	case <-done:
+		// Goroutine completed successfully
+	case <-time.After(15 * time.Second):
+		// Goroutine timed out
+		fmt.Println("Error: Request timed out")
+		os.Exit(1)
 	}
-
-	resp, err := client.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	var weatherData WeatherData
-	err = json.Unmarshal(data, &weatherData)
-	if err != nil {
-		panic(err)
-	}
-
-	tempCelsius := weatherData.Main.Temp - 273.15
-
-	sunriseTime := time.Unix(weatherData.Sys.Sunrise, 0).Format("15:04:05")
-	sunsetTime := time.Unix(weatherData.Sys.Sunset, 0).Format("15:04:05")
-
-	asciiSun := `
-      \   /
-       .-.
-    ‒ (   ) ‒
-       .-.
-      /   \
-  `
-	asciiCloud := `
-        .--.
-     .-(    ).
-    (___.__)__)
-  `
-	acsiiRainCloud := `
-      .--.
-   .-(    ).
-  (___.__)__)
-   ʻ‚ʻ‚ʻ‚ʻ‚ʻ
-  `
-	asciiSnow := `
-      .--.
-   .-(    ).
-  (___.__)__)
-   * * * * *
-  `
-
-	asciiThunder := `
-     .--.
-  .-(    ).
- (___.__)__)
-      /_
-       /
-  `
-
-	if strings.Contains(weatherData.Weather[0].Description, "sun") {
-		fmt.Println(asciiSun)
-	} else if strings.Contains(weatherData.Weather[0].Description, "rain") {
-		fmt.Println(acsiiRainCloud)
-	} else if strings.Contains(weatherData.Weather[0].Description, "snow") {
-		fmt.Println(asciiSnow)
-	} else if strings.Contains(weatherData.Weather[0].Description, "thunder") {
-		fmt.Println(asciiThunder)
-	} else {
-		fmt.Println(asciiCloud)
-	}
-
-	fmt.Printf("Location: %s\n", *location)
-	fmt.Printf("Weather: %s\n", weatherData.Weather[0].Description)
-	fmt.Printf("Temperature: %.2f°C\n", tempCelsius)
-	fmt.Printf("Sunrise: %s\n", sunriseTime)
-	fmt.Printf("Sunset: %s\n", sunsetTime)
 }
